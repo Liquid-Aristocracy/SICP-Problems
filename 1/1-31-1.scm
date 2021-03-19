dd883cdb53c15fb24abc0aace94947b3c64ee719
Liquid-Aristocracy <Na1048576@pm.me>
2021-03-19 19:03:09 +0800

    1-31

---
+++ 1/1-31-1.scm
@@ -0,0 +1,41 @@
+; input: 
+
+(define (product term a next b)
+    (if (> a b)
+        1
+        (*
+            (term a)
+            (product term (next a) next b)
+        )
+    )
+)
+
+(define (factorial a b)
+    (define (identity x)
+        x
+    )
+    (define (next x)
+        (+ x 1)
+    )
+    (product identity a next b)
+)
+
+(define (calc-pi a b)
+    (define (term x)
+        (*
+            (/ (- x 1) x)
+            (/ (+ x 1) x)
+        )
+    )
+    (define (next x)
+        (+ x 2)
+    )
+    (*
+        (product term a next b)
+        4
+    )
+)
+
+(display (calc-pi 3.0 (read)))
+(newline)
+        
---
+++ 1/1-31-2.scm
@@ -0,0 +1,44 @@
+; input: 
+
+(define (product term a next b)
+    (define (iter a result)
+        (if (> a b)
+            result
+            (iter 
+                (next a)
+                (* result (term a))
+            )
+        )
+    )
+    (iter a 1)
+)
+
+(define (factorial a b)
+    (define (identity x)
+        x
+    )
+    (define (next x)
+        (+ x 1)
+    )
+    (product identity a next b)
+)
+
+(define (calc-pi a b)
+    (define (term x)
+        (*
+            (/ (- x 1) x)
+            (/ (+ x 1) x)
+        )
+    )
+    (define (next x)
+        (+ x 2)
+    )
+    (*
+        (product term a next b)
+        4
+    )
+)
+
+(display (calc-pi 3.0 (read)))
+(newline)
+        
